13aa77dc0ec7a70e808e40165a9cc3336bf48f5e
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2023-09-26 19:53:25 +0200

    Add files via upload

--- src/components/buyNow/BuyNow.jsx
+++ src/components/buyNow/BuyNow.jsx
@@ -1,14 +1,22 @@
-import React from 'react'
 import "./buyNow.css"
+import { ShopContext } from "../../context/ShopContext";
+import { useContext } from "react";
 
-export default function BuyNow() {
+
+export default function BuyNow({cafe}) {
+
+  const { addToCart, cartItems } = useContext(ShopContext);
+  const cartItemCount = cartItems[cafe.id];
+
   return (
-    <div className='buy-now'>
+    <div className='buy-now' onClick={() => addToCart(cafe.id)} >
       <div>
         <div>
-          Add to Cart +
+          Add to Cart + {cartItemCount > 0 && <> ({cartItemCount})</>}
         </div>
       </div>
     </div>
   )
 }
+
+// onClick={console.log(cafe.id)}
