cba91a89f2c02fae45a6f44d226301d168476ce5
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2023-09-26 19:52:53 +0200

    Add files via upload

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -1,46 +1,85 @@
-import React, { createContext, useState } from 'react'
-import { db } from "../firebase.config"
-import { collection, getDocs } from "firebase/firestore"
+import { createContext, useEffect, useState } from "react";
+import { db } from "../firebase.config";
+import { collection, query } from "firebase/firestore";
 
+export const ShopContext = createContext(null);
 
-export const ShopContext = createContext(null)
+const coffeeRef = () => {
+    
+  const [ coffee, setCoffee ] = useState()
 
-const coffeeRef = collection(db, "coffee")
-
-//This function will get the array and create and object to represent the initial state of the cart
-const getDefaultCart = async () => {
-    let cart = {}
-    const snapshot = await getDocs(coffeeRef);
-    snapshot.docs.forEach((doc) => {
-      cart[doc.id] = 0;})
-      console.log(cart);
-    for (let i = 1; i < coffeeRef.length + 1; i++){
-        cart[i]= 0
-    }
-    return cart
+  useEffect(() => {
+    const coffeeRef = collection(db, "coffee")
+    const q = query(coffeeRef)
+    getDocs(q)
+      .then((querySnapshot) => {
+        let coffee = []
+        querySnapshot.docs.forEach((doc) => {
+          coffee.push({ ...doc.data(), id: doc.id})
+        })
+        setCoffee(coffee)
+      })
+      .catch(err => {
+      })
+     
+  }, [])
 }
 
+
+const getDefaultCart = () => {
+
+  let cart = {};
+  for (let i = 1; i < coffeeRef.length + 1; i++) {
+    cart[i] = 0;
+  }
+  return cart;
+};
 
 export const ShopContextProvider = (props) => {
 
-  const [ cartItems, setCartItems ] = useState(getDefaultCart([]))
+  const [cartItems, setCartItems] = useState(getDefaultCart());
 
+  const getTotalCartAmount = () => {
+    let totalAmount = 0;
+    for (const item in cartItems) {
+      if (cartItems[item] > 0) {
+        let itemInfo = coffeeRef.find((cafe) => cafe.id === Number(item));
+        totalAmount += cartItems[item] * itemInfo.price;
+      }
+    }
+    return totalAmount;
+  };
 
   const addToCart = (itemId) => {
-
-    setCartItems((prev) => ({...prev, [itemId]: prev[itemId] + 1 }))
-  }
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
+    console.log("HELLO");
+  };
+  
 
   const removeFromCart = (itemId) => {
-    
-    setCartItems((prev) => ({...prev, [itemId]: prev[itemId] - 1 }))
-  }
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
+  };
 
-  console.log(cartItems);
- 
-  const contextValue = { cartItems, addToCart, removeFromCart }
-  
-  return  <ShopContext.Provider value={contextValue}>{props.children}</ShopContext.Provider>
-}
+  const updateCartItemCount = (newAmount, itemId) => {
+    setCartItems((prev) => ({ ...prev, [itemId]: newAmount }));
+  };
 
+  const checkout = () => {
+    setCartItems(getDefaultCart());
+  };
 
+  const contextValue = {
+    cartItems,
+    addToCart,
+    updateCartItemCount,
+    removeFromCart,
+    getTotalCartAmount,
+    checkout,
+  };
+
+  return (
+    <ShopContext.Provider value={contextValue}>
+      {props.children}
+    </ShopContext.Provider>
+  );
+};
