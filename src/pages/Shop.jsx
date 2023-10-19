b41a8fa1e46e93b264714df4141a302076d85f9e
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2023-10-19 23:26:22 +0200

    Add files via upload

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,37 +1,18 @@
-import React, { useEffect, useState } from 'react'
-import { db } from "../firebase.config"
-import { collection , getDocs, query} from "firebase/firestore"
+import React from 'react'
 import Product from '../components/products/Product'
 import PageHeading from '../components/pageHeading/PageHeading'
 
 
 export default function Shop() {
 
-  const [ coffee, setCoffee ] = useState()
-
-  useEffect(() => {
-    const coffeeRef = collection(db, "coffee")
-    const q = query(coffeeRef)
-
-    getDocs(q)
-      .then((querySnapshot) => {
-        let coffee = []
-        querySnapshot.docs.forEach((doc) => {
-          coffee.push({ ...doc.data(), id: doc.id})
-        })
-
-        setCoffee(coffee)
-      })
-
-      .catch(err => {
-        console.log(err.message);
-      })
-  }, [])
-
   return (
     <div className='section shop'>
       <PageHeading text="All of our products" />
-      <Product coffee={coffee} />
+      <Product />
     </div>
   )
 }
+
+
+
+
