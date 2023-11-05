91aabe586f13feb98175ae806cf24d58065b8d81
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2023-11-05 23:00:44 +0100

    Add files via upload

---
+++ src/utils/useViewPortHeight.jsx
@@ -0,0 +1,21 @@
+import { useEffect } from 'react'
+
+//avoid scroll resize in Safari
+const useViewportheight = () => {
+    useEffect(() => {
+        const setVh = () => {
+            let vh = window.innerHeight * 0.01
+            document.documentElement.style.setProperty('--vh', `${vh}px`)
+        }
+
+        setVh()
+
+        window.addEventListener("resize", setVh)
+
+        return () => {
+            window.removeEventListener("resize", setVh)
+        }
+    }, [])
+}
+
+export default useViewportheight
