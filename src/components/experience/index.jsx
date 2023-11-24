061b93bfc75fb2b37bee2e6c796a6dd61b59d848
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2023-11-24 15:35:44 +0100

    Add files via upload

--- src/components/experience/index.jsx
+++ src/components/experience/index.jsx
@@ -1,4 +1,4 @@
-import React, { Suspense, useRef, useState } from 'react'
+import React, { Suspense, useRef, useState, useEffect } from 'react'
 import './experience.css'
 
 import { Canvas, useThree } from '@react-three/fiber'
@@ -11,27 +11,53 @@ import LandingPage from '../landingPage'
 import LoadingScreen from '../loadingScreen'
 
 
+function AspectWrapper(props) {
+  const { camera } = useThree();
+
+  const mql = window.matchMedia("(max-width: 930px)");
+  const mobileView = mql.matches;
+
+  useEffect(() => {
+    if (mobileView) {
+      camera.fov = 55;
+    } else {
+      camera.fov = 35;
+    }
+    camera.position.set(2.64, 1.18, 2.78);
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+  }, [camera, mobileView]);
+
+  return null;
+}
+
 export default function Experience() {
 
   const [ start, setStart ] = useState(false)
+  const [ isMobileView, setIsMobileView ] = useState(window.innerWidth < 930)
 
   const experienceRef = useRef(null)
 
-  const mobileView = window.innerWidth < 930
+  useEffect(() => {
+    const handleResize = () => setIsMobileView(window.innerWidth < 930);
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
 
   return (
     <>
       <div className="canvas" ref={experienceRef}>
-        <Canvas camera={{ fov: mobileView ? 55 : 35, position: [2.64, 1.18, 2.78] }}>
+        <Canvas key={isMobileView} camera={{ fov: isMobileView ? 55 : 35, position: [2.64, 1.18, 2.78] }}>
           <Suspense fallback={null}>
-            <ambientLight intensity={1} />
+            <ambientLight intensity={0.5} />
             <Environment preset="sunset" />
-            <PresentationControls polar={[0, 0]} azimuth={[-0.5, 0.5]}>
+            <PresentationControls global polar={[0, 0]} azimuth={[-0.5, 0.4]}>
               {start && 
                 <>
                   <CoffeeShop position-y={-0.3} /> 
                   <MenuLinks experienceRef={experienceRef} />
-                </>}
+                </>
+              }
             </PresentationControls>
           </Suspense>
         </Canvas>
@@ -55,16 +81,16 @@ function MenuLinks({ experienceRef }) {
 
   return (
     <>
-      <MenuLink position={[0, 0, 0]} rotation-y={1.6}>
-        <button className='shop' onClick={() => goToPage('/shop', [0.24, 0.2, 0.11])}>SHOP</button>
+      <MenuLink position={[-0.02, 0.1, -0.2]} rotation-y={1.6}>
+        <button className='shop-btn' onClick={() => goToPage('/shop', [0.24, 0.2, 0.11])}>SHOP</button>
       </MenuLink>
 
-      <MenuLink position={[-0.2, 0.4, -0.9]}>
-        <button className='about' onClick={() => goToPage('/about', [1.5, 1, 1.5])}>ABOUT</button>
+      <MenuLink position={[-0.2, 0.5, -0.95]}>
+        <button className='shop-btn' onClick={() => goToPage('/about', [1.5, 1, 1.5])}>ABOUT</button>
       </MenuLink>
 
-      <MenuLink position={[0.6, 0.6, -0.9]}>
-        <button className='contact' onClick={() => goToPage('/contact', [1, 1.1, -0.3])}>CONTACT</button>
+      <MenuLink position={[-0.14, 0.3, -0.95]}>
+        <button className='shop-btn' onClick={() => goToPage('/contact', [1, 1.1, -0.3])}>CONTACT</button>
       </MenuLink>
     </>
   )
@@ -77,5 +103,3 @@ function MenuLink({ children, ...props }) {
     </Html>
   )
 }
-
-
